cd4231d206358a14cea901c98fb4e2e9125b4c62
Ikiru Yoshizaki <guitarrapc@users.noreply.github.com>
2018-10-18 03:44:13 +0900

    change(go) apply auto format

--- src/Go/NewtonRaphthonMethod/main.go
+++ src/Go/NewtonRaphthonMethod/main.go
@@ -13,21 +13,21 @@ func Sqrt(x float64) float64 {
 	init := false
 	for {
 		i++
-	    prev = z
-        z -= (z * z - x ) / (2 * z)
-		if z == prev || (init && math.Abs(prev) < math.Abs(z + 0.00000000001)) {
-		    break
+		prev = z
+		z -= (z*z - x) / (2 * z)
+		if z == prev || (init && math.Abs(prev) < math.Abs(z+0.00000000001)) {
+			break
 		}
 		init = true
 	}
 	fmt.Println("input:", int64(x))
 	fmt.Println("count:", i)
-    return z
+	return z
 }
 
 func main() {
-    input := float64(67000000000)
-    result := Sqrt(input)
+	input := float64(67000000000)
+	result := Sqrt(input)
 	fmt.Println("try:", result)
 	fmt.Println("math:", math.Sqrt(input))
 }
