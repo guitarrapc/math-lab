d505b6d1c0cbbcbde861c40ae837520867951655
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2019-10-06 03:27:34 +0900

    fix: typo

--- src/CSharp/MathAlgorithm/Probability/ProbabilitySimple.cs
+++ src/CSharp/MathAlgorithm/Probability/ProbabilitySimple.cs
@@ -6,7 +6,7 @@ namespace MathAlgorithm.Probability
 {
     public static class ProbabilitySimple
     {
-        public static double tLeast1(double probability, int loop)
+        public static double AtLeast1(double probability, int loop)
             => Math.Pow((probability - 1) / probability, loop);
         public static double Continue1(double probability, int loop)
             => Math.Pow(1d - (probability - 1) / probability, loop);
